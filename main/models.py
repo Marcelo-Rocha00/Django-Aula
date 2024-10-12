441d6efbb45f716642858ae27aaf3d50af55f27f
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-11 21:11:14 -0300

    att

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length= 255)
+    telefone = models.CharField(max_length=15)
+    email = models.EmailField()
+    data_nascimento = models.DateField(null=True)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.nome
