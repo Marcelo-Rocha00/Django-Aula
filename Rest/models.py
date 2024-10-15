5bacc97e7539e5d082569347807d07513aa6828a
Marcelo-Rocha00 <marcelorochafilho04@gmail.com>
2024-10-15 20:05:57 -0300

    criação do serializers e das viewsets

--- Rest/models.py
+++ Rest/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Aluno(models.Model):
-    nome = models.CharField(max_length=200)
-    email = models.EmailField(unique=True)
-    idade = models.IntegerField()
-    matricula = models.CharField(max_length=20, unique=True)
-    data_matricula = models.DateField()
--- Rest/views.py
+++ Rest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from Serializers import AlunoSerializer
+from rest_framework import viewsets
+from main.models import Aluno
 # Create your views here.
+class AlunoViewSets(viewsets.ModelViewSet)
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
